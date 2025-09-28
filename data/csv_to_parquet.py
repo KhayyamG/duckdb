b6f743e98b5bd60b4c39d59c2be4de1fd86c3d1e
KhayyamG <khayyamgahraman@gmail.com>
2025-09-28 20:13:47 +0400

    removed comments

--- data/csv_to_parquet.py
+++ data/csv_to_parquet.py
@@ -7,12 +7,12 @@ df = pd.read_csv(csv_path)
 target_size = 2 * 1024**3
 
 current_size = os.path.getsize(csv_path)
-print(f"Orijinal CSV boyutu: {current_size/1024**2:.2f} MB")
+print(f"CSV size: {current_size/1024**2:.2f} MB")
 
 repeat_factor = int(target_size // current_size) + 1
-print(f"Tekrarlama katsayısı: {repeat_factor}")
+print(f"dublicate counts: {repeat_factor}")
 
-big_df = pd.concat([df] * 30, ignore_index=True)
+big_df = pd.concat([df] * repeat_factor, ignore_index=True)
 
 big_df.to_parquet("D:\lakehouse_demo\duckdb\data\customers-2gb.parquet", engine="pyarrow", index=False)
-print("customers-2gb.parquet oluşturuldu ✅")
+print("customers-2gb.parquet created")
